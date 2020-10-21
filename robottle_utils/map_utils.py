dbce1666bfef3fb76487acefcb8d33f3926e0507
Arthur Bricq <arthur.bricq@epfl.ch>
2020-10-21 15:14:47 +0200

    Adding function get_map

--- robottle_utils/map_utils.py
+++ robottle_utils/map_utils.py
@@ -1,2 +1,7 @@
-def get_map(map_as_array):
-    return "Here is your map"
+import numpy as np
+
+def get_map(map_as_array, width, height):
+    """
+    Returns the map as a 2D occupancy grid (numpy) from the long format of the map.
+    """
+    return np.array(map_as_array).reshape(width, height)
