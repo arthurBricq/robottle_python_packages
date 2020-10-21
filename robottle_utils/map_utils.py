a6151f6bea2e0cc841235415abb8e27d39336293
Arthur Bricq <arthur.bricq@epfl.ch>
2020-10-21 18:09:34 +0200

    Setting parameters for real deployment

--- robottle_utils/map_utils.py
+++ robottle_utils/map_utils.py
@@ -1,10 +1,10 @@
 import numpy as np
 
-def get_map(map_as_array, width, height):
+def get_map(map_as_array, n_w = 500, n_h = 500):
     """
     Returns the map as a 2D occupancy grid (numpy) from the long format of the map.
     """
-    return np.array(map_as_array).reshape(width, height)
+    return np.array(map_as_array).reshape(n_w, n_h)
 
 def inspect_line(occupancy_grid, robot_position, length, w=10, h=10):
     """
@@ -18,20 +18,20 @@ def inspect_line(occupancy_grid, robot_position, length, w=10, h=10):
 
     TODO: length out of bounds
     """
-    (x,y,theta) = robot_pos
-    n_x, n_y = occupancy_grid.shape
+    (x,y,theta) = robot_position
+    theta = np.deg2rad(theta)
+    n_w, n_h = occupancy_grid.shape
 
-    robot_x = int(robot_pos[0] / w * n_x)
-    robot_y = int(robot_pos[1] / h  * n_y)
+    robot_x = int(x / w * n_w)
+    robot_y = int(y / h  * n_h)
 
-    length_in_pixel = int(length / w * n_x)
+    length_in_pixel = int(length / w * n_w)
     x_dir = robot_x
     y_dir = robot_y
     for _ in range(length_in_pixel):
         x_dir += np.cos(theta)
         y_dir += np.sin(theta)
-        robot_dir_index = (round(x_dir), round(y_dir))
-        if occupancy_grid[robot_dir_index] > 125:
+        robot_dir_index = (int(round(y_dir)), int(round(x_dir)))
+        if occupancy_grid[robot_dir_index] < 100:
             return True
     return False
-
