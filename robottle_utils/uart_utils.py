15b4c71aaa1215647afe9bca79416c3397f94336
Arthur Bricq <arthur.bricq@epfl.ch>
2020-11-02 19:25:53 +0100

    adding uart_utils with updated optimized function

--- robottle_utils/uart_utils.py
+++ robottle_utils/uart_utils.py
@@ -4,10 +4,4 @@ def get_speed(datas):
 From the array of characters read by UART, 
 returns the speed of the wheels as a number, if possible.
     """
-    if datas[0] == '-':
-        value = - float(''.join(datas[1:]))
-    else:
-        value = float(''.join(datas[:]))
-    return value
-    
-
+    return float(''.join(datas[:]))
