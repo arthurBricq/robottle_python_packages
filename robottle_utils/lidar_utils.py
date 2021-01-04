3626bfefdf16824bd55d3ee4beaa2793550b10a8
Arthur Bricq <arthur.bricq@epfl.ch>
2021-01-04 11:09:35 +0100

    updates

--- robottle_utils/lidar_utils.py
+++ robottle_utils/lidar_utils.py
@@ -34,7 +34,7 @@ def check_obstacle_ahead(distances, angles, save_index = None, length_to_check =
         np.save("/home/arthur/dev/ros/data/lidar/distances_{}.npy".format(save_index), distances)
     
     # return true depending on the obstacle detection
-    print("lidar count: ", np.count_nonzero(obstacles1) + np.count_nonzero(obstacles2))
+    print("lidar count: ", np.count_nonzero(obstacles1) , np.count_nonzero(obstacles2))
     to_return = (np.count_nonzero(obstacles1) + np.count_nonzero(obstacles2)) >= 8
 
     return to_return
